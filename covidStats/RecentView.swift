e3274942667d1a9399b51f6a930fe91ac89d29b8
levi davis <ldd235@gmail.com>
2020-10-08 15:15:00 -0400

    Adds tab bar with recent tab

---
+++ covidStats/RecentView.swift
@@ -0,0 +1,24 @@
+//
+//  RecentView.swift
+//  covidStats
+//
+//  Created by Levi Davis on 10/8/20.
+//  Copyright © 2020 Levi Davis. All rights reserved.
+//
+
+import SwiftUI
+
+struct RecentView: View {
+    
+    @ObservedObject var covidFetch = COVIDFetchRequest()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecentView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecentView()
+    }
+}
